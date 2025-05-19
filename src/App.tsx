64d5ecfcca823af4f050a97ee7c7e2414831cb3a
ayushvadodariya <ayushvadodariya12@gmail.com>
2025-05-19 22:42:02 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
 
   return (
-    <h1> Welcome to elib dashboard</h1>
+    <>
+      <h1> Welcome to elib dashboard</h1>
+      <Button>Click me!!!</Button>
+    </>
+
   )
 }
 
